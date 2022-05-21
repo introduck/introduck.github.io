a47e8b3079b8de8bebc1d185de53424c53defe85
speqtr <speqtr@users.noreply.github.com>
2022-05-21 19:44:20 +0000

    Fixed 'npm run build'

---
+++ docs/assets/404-legacy.5a854d2f.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy.b0b9d37e.js"],(function(e){"use strict";var t,n,r;return{setters:[function(e){t=e._,n=e.o,r=e.b}],execute:function(){e("default",t({},[["render",function(e,t){return n(),r("p",null,"404")}]]))}}}));
---
+++ docs/assets/404.ce1edea1.js
@@ -0,0 +1,1 @@
+import{_ as e,o as r,b as c}from"./index.ef57ce15.js";const n={};function o(t,_){return r(),c("p",null,"404")}var s=e(n,[["render",o]]);export{s as default};
